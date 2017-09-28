8ccd9a384722afb42e69f2fb3ccd04f563adb821
aidanmccoy <aidanmccoy2@gmail.com>
2017-09-27 21:30:47 -0700

    Started Mergesort

--- Program1/Runtime.java
+++ Program1/Runtime.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
+import java.lang.*;
 
 public class Runtime {
+
 	public static int[] SelectionSort(int[] input) {
 		int minNdx, tempInt, j, i;
 		for (i = 0; i < (input.length -1); i++) {
@@ -17,9 +19,34 @@ public class Runtime {
 		return input;
 	}
 
-	/*public static int[] MergeSort(int[] input) {
-		
-	}*/
+	public static int[] MergeSort(int[] input) {
+		int mid, n;
+		int[] lArray, rArray;
+	
+		n = input.length;
+
+		if (n <= 1) {
+			return input;
+		} else {
+			mid = n/2;
+
+			lArray = new int[mid];
+			rArray = new int[n - mid];
+
+			System.arraycopy(input, 0, lArray, 0, mid);
+			System.arraycopy(input, mid + 1, rArray, 0, n - mid);
+
+			return Merge(MergeSort(lArray), MergeSort(rArray));
+		}
+	}
+
+	public static int[] Merge (int[] input1, int[] input2) {
+		if (input1.length == 0) {
+			return input2;
+		} else if (input2.length == 0) {
+			return input1;
+		} 
+	}
 
 	public static void main(String[] args) {
 		int[] testData = new int[] {5,2,7,4,2,6,9,0,4,6,7,3,1,3,8,9,7,5,6,3,4};
