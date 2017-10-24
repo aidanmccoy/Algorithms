52601acb33f6f824ed715bfa183fcf7d5caad606
Aidan <aidanmccoy2@gmail.com>
2017-10-24 10:54:33 -0700

    Camper class

---
+++ Program3/Camper.java
@@ -0,0 +1,30 @@
+public class Camper {
+	private int swimtime;
+	private int runtime;
+	private int biketime;
+
+	private void setSwimtime(int time) {
+		this.swimtime = time;
+	}
+
+	private void setRuntime(int time) {
+		this.runtime = time;
+	}
+
+	private void setBiketime(int time) {
+		this.biketime = time;
+	}
+
+	private int getSwimtime() {
+		return this.swimtime;
+	}
+
+	private int getRuntime() {
+		return this.runtime;
+	}
+
+	private int getBiketime() {
+		return this.biketime;
+	}
+
+}
---
+++ Program3/Triathalon.java
@@ -0,0 +1,8 @@
+public class Triathalon {
+
+	public static void main(String[] args) throws IOException {
+		ArrayList<Camper> = new ArrayList()<Camper>;
+		File file = new File(args[0]);
+
+			
+	}
